f93ca1cfce894a6c2326ac75d42d7b5ada9a146d
Praveen <pkchmits@gmail.com>
2019-11-27 06:24:05 +0530

    Step 1, adding joke telling java library done

---
+++ JokeTellingLib/src/main/java/com/sabkayar/praveen/joketellinglib/Joke.java
@@ -0,0 +1,7 @@
+package com.sabkayar.praveen.joketellinglib;
+
+public class Joke {
+    public String getJoke(){
+        return "Joke from Java Library";
+    }
+}
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.sabkayar.praveen.joketellinglib.Joke;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,7 +42,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view) {
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(this, new Joke().getJoke(), Toast.LENGTH_SHORT).show();
     }
 
 
